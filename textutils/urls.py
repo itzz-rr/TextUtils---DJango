97f16a5262f531a717549fafc7f92a196ee9211f
Rishi <rishiraj11a@gmail.com>
2022-11-04 21:17:29 +0530

    updating css

--- textutils/urls.py
+++ textutils/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('exercise1', views.exercise1, name='Exercise-1'),
     path('analyze', views.analyze, name='Analyzed Text')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
